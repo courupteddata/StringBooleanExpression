e81a3db9748b1afdb9a6a99d3c70b71a382558ec
nathan <nathan@jones.one>
2020-02-22 20:45:36 -0500

    Adding readme and some comments

---
+++ readme.md
@@ -0,0 +1,33 @@
+#String Boolean Expression
+Ever wanted a maybe safe way to resolve or take in a boolean expression from the user or a configuration.
+
+The input of:
+S$employee_name==BoB||(F$salary<<20.5||F&salary>=100)
+Is resolved to handle check if employees name equals BoB or the salary is in the range
+
+##Comparison Map
+|Input  	|Resolved comparison  	|
+|---	|---	|
+|==  	|equals  	|
+|!=  	|not equal  	|
+|\>>  	|greater than  	|
+|<<  	|less than  	|
+|>=  	|greater than or equal  	|
+|<=  	|less than or equal  	|
+
+##Type Map
+|Input  	|Resolved type  	|
+|---	|---	|
+|S  	|string  	|
+|I  	|integer  	|
+|F  	|float  	|
+
+##Operator Map
+|Input  	|Resolved type  	|
+|---	|---	|
+|&#124;&#124;  	|or  	|
+|&& 	|and  	|
+|( 	|open grouping  	|
+|) 	|close grouping  	|
+#Disclaimer
+A best effort was done to restrict possible malicious input and if it is found then a ValueError is raised that needs to be caught.
--- stringbooleanexpression.py
+++ stringbooleanexpression.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2019 Nathan
+Copyright (c) 2020 Nathan
 Original available at https://github.com/courupteddata/StringBooleanExpression
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -26,10 +26,7 @@ This is a class that handles loading in a string and can parse passed in variabl
 This allows loading in a file that has boolean expressions.
 
 Limitations:
-variable to be replaced must always be on the left of the comparison operation
-    ie. (I$FieldOne>=5)&&
-
-    !!((I$a==5)&&(S$b!=other))
+In the event of a invalid string being found in the input_string a ValueError is raised.
 
 """
 
@@ -63,11 +60,22 @@ ILLEGAL_STRINGS = ["import ", "eval", "\n", "\r", "\"", "\'",
                    "lambda", " if ", " else ", "for ", "def ",
                    "while", ":"]
 
+# Used to wrap internal variables so they are unique
 INTERNAL_VARIABLE_WRAP_CHAR = "__"
 
 
 class StringBooleanExpression:
     def __init__(self, input_string: str):
+        """
+        NOTE: This can raise a ValueError in the event that the input_string was invalid.
+
+        Initializes/Loads the string boolean expression that is represented as a string.
+        The input string should have pieces of the format DataType$field_name[operator]value .
+        For example: S$employee_name==BoB||F$salary<<20.5
+                     This would return true if employee_name equals BoB or if the salary is less than 20.5
+
+        :param input_string: The string that contains the
+        """
         self._check_for_invalid(input_string)
         self._command_string, self._variables = self._parse_input(input_string)
 
@@ -75,6 +83,11 @@ class StringBooleanExpression:
         self._command = self._set_up_function(self._command_string, self._sorted_variables)
 
     def check(self, input_dict: dict) -> bool:
+        """
+        If the input dictionary is missing the variables required to resolve the expression then False is returned
+        :param input_dict: The dictionary to contain the fields to look for
+        :return: If the expression resolves successfully then True, otherwise False (Also False if missing expression fields)
+        """
         if all(variable in input_dict for variable in self._sorted_variables):
             return self._command(*[input_dict[item] for item in self._sorted_variables])
         else:
@@ -82,6 +95,13 @@ class StringBooleanExpression:
 
     @staticmethod
     def _set_up_function(command_string: str, sorted_variables: list):
+        """
+        This handles actually creating the command to run given a parsed command string and the sorted variables
+        :param command_string: The parsed command string with the variables casted/replaced, and operators adjusted
+        :param sorted_variables: The variables in their expected order
+        :return: An evaluated expression that can be called given valid input
+        """
+
         input_sequence = ", ".join([INTERNAL_VARIABLE_WRAP_CHAR + item + INTERNAL_VARIABLE_WRAP_CHAR
                                     for item in sorted_variables])
         command = eval("lambda " + input_sequence + ": " + command_string, {})
@@ -89,6 +109,11 @@ class StringBooleanExpression:
 
     @staticmethod
     def _parse_input(input_string: str) -> (str, set):
+        """
+        Parses the input based off of the comparisons
+        :param input_string: A string that has already passed basic safty checks
+        :return: A string with the valid operators and the operators that have been corrected
+        """
 
         # Handle comparisons first
         variables_hold = set()
@@ -105,6 +130,13 @@ class StringBooleanExpression:
 
     @staticmethod
     def _handle_comparison(input_string: str, comparison_operator: str, replacement: str) -> (str, set):
+        """
+        Splits based off of a comparison operator and the handles the left and right side of the operator
+        :param input_string:
+        :param comparison_operator: This would be index zero from and entry in COMPARISONS
+        :param replacement: This would index one from and entry in COMPARISONS
+        :return: Updated string and a set of the variables found
+        """
         variables = set()
 
         parts = input_string.split(comparison_operator)
@@ -214,13 +246,20 @@ class StringBooleanExpression:
 
     @staticmethod
     def _wrap_string(value: str, wrap: str, condition: bool) -> str:
+        """
+        Given a condition this function will wrap a value
+        :param value: The value to wrap
+        :param wrap: The string to use to wrap the value
+        :param condition: The condition to base the wrap logic off of
+        :return: Either a wrapped string if condition is True, otherwise it just returns the input value
+        """
         return (wrap if condition else "") + value + (wrap if condition else "")
 
     @staticmethod
     def _check_for_invalid(input_string: str) -> None:
         """
         Will check the input string for invalid strings that could be a sign of a malicious actor
-        @param input_string: THe command string to modify
+        @param input_string: The command string to modify
         @return: None, but will raise a ValueError on something invalid being found
         """
         invalid_found = []
